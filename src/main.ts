4cae79cb9fc01ad62190a2fd864bc4e21dd508ee
Paula1670 <paupulpeg@gmail.com>
2024-07-24 23:11:29 +0200

    FixedDB

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // Configurar el tamaño máximo permitido para las cargas útiles
